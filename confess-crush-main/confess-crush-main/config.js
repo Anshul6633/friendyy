ce0216c50a2a906cd2242bcc5736fdf3f3aff943
Anshul6633 <166302727+Anshul6633@users.noreply.github.com>
2024-10-21 21:32:25 +0530

    Update config.js

--- confess-crush-main/confess-crush-main/config.js
+++ confess-crush-main/confess-crush-main/config.js
@@ -6,9 +6,7 @@ const CONFIG = {
                     `,
     btnIntro: 'so to listen more things nice about you to idharrr clicckkk kariyooooooooooo',
     title: '',
-    desc: "currently the best thinggggg about u is you are trying hard asf to get back in the postion you were..."
-            'There are lot of thing to speak but its not write to speak that thing kabhii karengee baat aram sae!!'
-            "aur yaad rakhi khiyo 6months mae internship keliye!"
+    desc: 'currently the best thinggggg about u is you are trying hard asf to get back in the postion you were.There are lot of thing to speak but its not write to speak that thing kabhii karengee baat aram sae aur yaad rakhi khiyo 6months mae internship keliye!',
     question: ' you know what?? ',
     btnReply: 'To jerruuu',
     reply: 'u are kind of best person i have ever met!!',
