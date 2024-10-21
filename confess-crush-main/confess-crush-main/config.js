c29639a1a6906e6ef5b6fd7258a63f3cb6800af6
ANSHUL6633 <khatrianshul977@gmail.com>
2024-10-21 21:03:21 +0530

    first commit

---
+++ confess-crush-main/confess-crush-main/config.js
@@ -0,0 +1,20 @@
+const CONFIG = {
+    titleWeb: "Tiêu đề web",
+    introTitle: 'SRUSHTI',
+    introDesc: `tiny little girl
+    anger on her face but there is hidden smile after every cold look!
+    this make you the rarest amongs the all
+    girl with a anger on her nose!!`,
+    btnIntro: '^^HiHi^^',
+    title: 'hey srush from last few days i wanna say something!',
+    desc: 'i l like you since the day iwas you!',
+    btnYes: 'YES <33',
+    btnNo: 'No better luck next  time',
+    question: ' There are more than 7 billion people in the world and why do you love yourself ',
+    btnReply: 'To you <3',
+    reply: 'To love is to love but not to love is to love <33333333',
+    mess: 'I know. 🥰 Love you so much 😘😘',
+    messDesc: 'Tonight at 7 oclock, I m over to pick you up.',
+    btnAccept: 'Okiiiii lovee <3',
+    messLink: 'https://github.com/zukahai/Confess-Crush' //link mess của các bạn. VD: https://www.facebook.com/messages/t/100014188333536
+}
